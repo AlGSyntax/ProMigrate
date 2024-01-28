f10f0f1c8cd405be5a8d4358ff3989a1f8ed6d0e
alvaro <adelcastifo@gmail.com>
2024-01-28 15:06:51 +0100

    The method updateConfiguration was deprecated, so I replaced the methods applySavedLanguage and setLocale with attachBaseContext to locally store the language and localization settings

--- app/src/main/java/com/example/promigrate/MainActivity.kt
+++ app/src/main/java/com/example/promigrate/MainActivity.kt
@@ -1,31 +1,29 @@
 package com.example.promigrate
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+
+    override fun attachBaseContext(newBase: Context?) {
+        val sharedPrefs = newBase?.getSharedPreferences("AppSettings", Context.MODE_PRIVATE)
+        val languageCode = sharedPrefs?.getString("SelectedLanguage", Locale.getDefault().language)
+        val locale = languageCode?.let { Locale(it) }
+        if (locale != null) {
+            Locale.setDefault(locale)
+        }
+
+        val config = newBase?.resources?.configuration
+        config?.setLocale(locale)
+
+        val context = newBase?.createConfigurationContext(config!!)
+        super.attachBaseContext(context)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        applySavedLanguage()
-
         setContentView(R.layout.activity_main)
-    }
-
-    private fun applySavedLanguage() {
-        val sharedPrefs = getSharedPreferences("AppSettings", Context.MODE_PRIVATE)
-        val languageCode = sharedPrefs.getString("SelectedLanguage", Locale.getDefault().language)
-        setLocale(languageCode)
-    }
-
-    private fun setLocale(languageCode: String?) {
-        languageCode?.let {
-            val locale = Locale(it)
-            Locale.setDefault(locale)
-            val config = resources.configuration
-            config.setLocale(locale)
-            resources.updateConfiguration(config, resources.displayMetrics)
-        }
     }
 }
--- app/src/main/java/com/example/promigrate/data/local/ProMigrateDatabase.kt
+++ app/src/main/java/com/example/promigrate/data/local/ProMigrateDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.promigrate.data.model.UserProfile
 
-@Database(entities = [UserProfile::class], version = 1)
+@Database(entities = [UserProfile::class], version = 1, exportSchema = false)
 abstract class UserDatabase : RoomDatabase() {
     abstract fun userProfileDao(): UserProfileDao
 
