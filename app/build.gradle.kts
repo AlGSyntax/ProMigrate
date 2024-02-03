d49987b51b007afc16faa3b4367545bd8c0a93ca
alvaro <adelcastifo@gmail.com>
2024-02-03 16:43:53 +0100

    Increasing the targetSdkVersion from 24 to 28.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.promigrate"
-        minSdk = 24
+        minSdk = 28
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
