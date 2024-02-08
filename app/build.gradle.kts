92634441f232181a8d1ddb68bce5ab4dd08108d0
alvaro <adelcastifo@gmail.com>
2024-02-08 09:02:57 +0100

    Added WorkManager-Dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,6 +94,9 @@ dependencies {
     //Google Sign In
     implementation ("com.google.android.gms:play-services-auth:20.7.0")
 
+    //WorkManager
+    implementation ("androidx.work:work-runtime-ktx:2.9.0")
+
 
 
 
