cd0cbcae56d19b780b3d97cca47c5846d437c313
alvaro <adelcastifo@gmail.com>
2024-03-11 16:17:49 +0100

    Adding documentation and comments in the DashboardFragment is finished for the time being.

--- app/src/main/java/com/example/promigrate/ui/DashboardFragment.kt
+++ app/src/main/java/com/example/promigrate/ui/DashboardFragment.kt
@@ -14,21 +14,41 @@ import com.example.promigrate.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
 
-    private lateinit var binding: FragmentDashboardBinding
+    // Die Verwendung von activityViewModels() bietet Zugriff auf das ViewModel, das von der
+    // zugehörigen Activity genutzt wird.
     private val viewModel: MainViewModel by activityViewModels()
 
+    // Binding-Objekt für das DashboardFragment, ermöglicht den einfachen Zugriff auf
+    // die UI-Komponenten.
+    private lateinit var binding: FragmentDashboardBinding
+
+    // 'args' wird verwendet, um die Argumente zu holen, die an das DashboardFragment übergeben wurden.
+    // Die Verwendung von 'navArgs()' ermöglicht den sicheren und typisierten Zugriff auf diese Argumente.
     private val args: DashboardFragmentArgs by navArgs()
 
+    /**
+     * Initialisiert die Benutzeroberfläche des Fragments, indem das entsprechende Layout aufgeblasen wird und
+     * die Google-Anmeldekonfiguration vorgenommen wird.
+     *
+     * @param inflater: Das LayoutInflater-Objekt, das zum Aufblasen des Layouts des Fragments verwendet wird.
+     * @param container: Die übergeordnete ViewGroup, in den die neue Ansicht eingefügt wird.
+     * @param savedInstanceState: Ein Bundle, das den Zustand des Fragments gespeichert hat.
+     * @return :Die erstellte View für das Fragment.
+     */
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        // Initialisiert das Binding für das Fragment, um auf die UI-Elemente zugreifen zu können.
         binding = FragmentDashboardBinding.inflate(inflater, container, false)
 
         markOnboardingComplete()
 
+        // Beobachtet die userProfileData LiveData im ViewModel.
+        // Wenn Daten vorhanden sind, werden bestimmte UI-Elemente sichtbar gemacht.
         viewModel.userProfileData.observe(viewLifecycleOwner) { userProfile ->
             if (userProfile != null) {
+                // Setzt die Sichtbarkeit der Karten auf sichtbar, sobald die Benutzerdaten geladen sind.
                 binding.cardTopLeft.visibility = View.VISIBLE
                 binding.cardTopRight.visibility = View.VISIBLE
                 binding.cardBottomLeft.visibility = View.VISIBLE
@@ -36,102 +56,118 @@ class DashboardFragment : Fragment() {
             }
         }
 
+        // Setzt einen OnClickListener für die untere linke Karte im Dashboard.
         binding.cardBottomLeft.setOnClickListener {
-            // Check if userProfileData is already loaded
+            // Überprüft, ob userProfileData bereits geladen ist.
             val userProfile = viewModel.userProfileData.value
             if (userProfile?.selectedJobs != null) {
-                // If userProfileData is loaded, use it
+                // Verwendet die geladenen Benutzerprofildaten, wenn verfügbar.
                 val jobTitlesArray = userProfile.selectedJobs!!.keys.toTypedArray()
                 val refNrsArray = userProfile.selectedJobs!!.values.toTypedArray()
                 val arbeitsort = userProfile.desiredLocation ?: ""
 
-                val action = DashboardFragmentDirections.actionDashboardFragmentToDetailToDoJobApplicationFragment(
-                    selectedJobTitles = jobTitlesArray,
-                    selectedJobRefNrs = refNrsArray,
-                    arbeitsort = arbeitsort
-                )
-                findNavController().navigate(action)
-            }else {
-                // Im 'else'-Teil: Verwende die Argumente aus den FragmentArgs
-                val selectedJobsArray = args.selectedJobTitles ?: arrayOf()
-                val refNrsArray = args.selectedJobRefNrs ?: arrayOf()
-
-                val arbeitsort = args.arbeitsort ?: ""
-
-                val action = DashboardFragmentDirections.actionDashboardFragmentToDetailToDoJobApplicationFragment(
-                    selectedJobTitles = selectedJobsArray,
-                    selectedJobRefNrs = refNrsArray,
-                    arbeitsort = arbeitsort
-                )
-                findNavController().navigate(action)
-            }
-        }
-
-
-        binding.cardTopLeft.setOnClickListener {
-            val userProfile = viewModel.userProfileData.value
-            if (userProfile?.selectedJobs != null) {
-                // If userProfileData is loaded, use it
-                val jobTitlesArray = userProfile.selectedJobs!!.keys.toTypedArray()
-                val refNrsArray = userProfile.selectedJobs!!.values.toTypedArray()
-                val arbeitsort = userProfile.desiredLocation ?: ""
-
-                val action = DashboardFragmentDirections.actionDashboardFragmentToDetailToDoJobResearchFragment(
-                    selectedJobTitles = jobTitlesArray,
-                    selectedJobRefNrs = refNrsArray,
-                    arbeitsort = arbeitsort
-                )
+                // Erstellt eine Navigationsaktion mit den gesammelten Informationen.
+                val action =
+                    DashboardFragmentDirections.actionDashboardFragmentToDetailToDoJobApplicationFragment(
+                        selectedJobTitles = jobTitlesArray,
+                        selectedJobRefNrs = refNrsArray,
+                        arbeitsort = arbeitsort
+                    )
                 findNavController().navigate(action)
             } else {
-                // Im 'else'-Teil: Verwende die Argumente aus den FragmentArgs
+                // Verwendet die Argumente des Fragments als Fallback, wenn keine Benutzerdaten vorhanden sind
                 val selectedJobsArray = args.selectedJobTitles ?: arrayOf()
                 val refNrsArray = args.selectedJobRefNrs ?: arrayOf()
-
                 val arbeitsort = args.arbeitsort ?: ""
 
-                val action = DashboardFragmentDirections.actionDashboardFragmentToDetailToDoJobResearchFragment(
-                    selectedJobTitles = selectedJobsArray,
-                    selectedJobRefNrs = refNrsArray,
-                    arbeitsort = arbeitsort
-                )
+
+                // Erstellt eine Navigationsaktion mit den Argumenten des Fragments.
+                val action =
+                    DashboardFragmentDirections.actionDashboardFragmentToDetailToDoJobApplicationFragment(
+                        selectedJobTitles = selectedJobsArray,
+                        selectedJobRefNrs = refNrsArray,
+                        arbeitsort = arbeitsort
+                    )
                 findNavController().navigate(action)
             }
         }
 
-        binding.cardTopRight.setOnClickListener {
+        // Setzt einen OnClickListener für die obere linke Karte im Dashboard.
+        binding.cardTopLeft.setOnClickListener {
+            // Überprüft, ob userProfileData bereits geladen ist.
+            val userProfile = viewModel.userProfileData.value
+            if (userProfile?.selectedJobs != null) {
+                // Verwendet die geladenen Benutzerprofildaten, wenn verfügbar.
+                val jobTitlesArray = userProfile.selectedJobs!!.keys.toTypedArray()
+                val refNrsArray = userProfile.selectedJobs!!.values.toTypedArray()
+                val arbeitsort = userProfile.desiredLocation ?: ""
 
-                // Verwende Standardwerte oder zeige eine Fehlermeldung/Logik, wenn keine Daten vorhanden sind
-                val action = DashboardFragmentDirections.actionDashboardFragmentToVocabularyLearningFragment()
+                // Erstellt eine Navigationsaktion mit den gesammelten Informationen.
+                val action =
+                    DashboardFragmentDirections.actionDashboardFragmentToDetailToDoJobResearchFragment(
+                        selectedJobTitles = jobTitlesArray,
+                        selectedJobRefNrs = refNrsArray,
+                        arbeitsort = arbeitsort
+                    )
+                findNavController().navigate(action)
+            } else {
+                // Verwendet die Argumente des Fragments als Fallback, wenn keine Benutzerdaten vorhanden sind
+                val selectedJobsArray = args.selectedJobTitles ?: arrayOf()
+                val refNrsArray = args.selectedJobRefNrs ?: arrayOf()
+                val arbeitsort = args.arbeitsort ?: ""
+
+                // Erstellt eine Navigationsaktion mit den Argumenten des Fragments.
+                val action =
+                    DashboardFragmentDirections.actionDashboardFragmentToDetailToDoJobResearchFragment(
+                        selectedJobTitles = selectedJobsArray,
+                        selectedJobRefNrs = refNrsArray,
+                        arbeitsort = arbeitsort
+                    )
                 findNavController().navigate(action)
             }
+        }
 
+
+        // Setzt einen OnClickListener für die obere rechte Karte im Dashboard.
+        binding.cardTopRight.setOnClickListener {
+
+            // Erstellt eine Navigationsaktion zur VocabularyLearningFragment.
+            val action =
+                DashboardFragmentDirections.actionDashboardFragmentToVocabularyLearningFragment()
+            findNavController().navigate(action)
+        }
+
+        // Setzt einen OnClickListener für die untere rechte Karte im Dashboard.
         binding.cardBottomRight.setOnClickListener {
-            val action= DashboardFragmentDirections.actionDashboardFragmentToRelocationAndIntegrationFragment()
+
+            // Erstellt eine Navigationsaktion zur RelocationAndIntegrationFragment.
+            val action =
+                DashboardFragmentDirections.actionDashboardFragmentToRelocationAndIntegrationFragment()
             findNavController().navigate(action)
         }
 
 
-
-
-
-
-
-
-        binding.imageView.setOnClickListener {
+        // Setzt einen OnClickListener für das Profilbild im Dashboard.
+        binding.tosettingsfragbtn.setOnClickListener {
+            // Erstellt eine Navigationsaktion zum SettingsFragment.
             val action = DashboardFragmentDirections.actionDashboardFragmentToSettingsFragment()
             findNavController().navigate(action)
         }
 
 
-
+        // Gibt die erstellte View für das Fragment zurück.
         return binding.root
     }
 
-
+    // Markiert den Onboarding-Prozess als abgeschlossen, indem ein entsprechender Wert in den Shared Preferences gespeichert wird.
     private fun markOnboardingComplete() {
-        val sharedPref = activity?.getSharedPreferences("MyAppPreferences", Context.MODE_PRIVATE) ?: return
+        // Zugriff auf die Shared Preferences der App
+        val sharedPref =
+            activity?.getSharedPreferences("MyAppPreferences", Context.MODE_PRIVATE) ?: return
         with(sharedPref.edit()) {
+            // Setzt den Wert von "OnboardingComplete" auf true und speichert die Änderungen.
             putBoolean("OnboardingComplete", true)
+            // Wendet die Änderungen an die Shared Preferences an.
             apply()
         }
     }
