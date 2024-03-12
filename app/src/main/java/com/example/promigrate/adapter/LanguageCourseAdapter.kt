8e7ce1a5d7473c39eb8bb4adf0415cddd94ec3c6
alvaro <adelcastifo@gmail.com>
2024-03-12 10:24:41 +0100

    Adding documentation and comments in the LanguageCourseFragment and the LanguageCourseAdapter finished .

--- app/src/main/java/com/example/promigrate/adapter/LanguageCourseAdapter.kt
+++ app/src/main/java/com/example/promigrate/adapter/LanguageCourseAdapter.kt
@@ -18,41 +18,85 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.promigrate.R
+import com.example.promigrate.adapter.LanguageCourseAdapter.DiffCallback
 import com.example.promigrate.data.model.TerminResponse
 import com.example.promigrate.databinding.LanguageCourseItemBinding
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-class LanguageCourseAdapter : ListAdapter<TerminResponse, LanguageCourseAdapter.LanguageCourseViewHolder>(DiffCallback) {
+/**
+ * LanguageCourseAdapter ist eine Unterklasse von ListAdapter.
+ * Dieser Adapter ist verantwortlich für die Darstellung und Interaktion mit der Liste der Sprachkurse in der Anwendung.
+ * Er stellt eine Liste von TerminResponse-Objekten dar, die die Daten für jeden Sprachkurs enthalten.
+ * Die Daten für die Sprachkurse werden von einem ViewModel bereitgestellt.
+ *
+ * @property DiffCallback: Ein DiffUtil.ItemCallback, der zwei TerminResponse-Objekte vergleicht, um effiziente Updates in der RecyclerView zu ermöglichen.
+ * Dieser Callback optimiert die Aktualisierungen, indem er nur die geänderten Elemente neu rendert.
+ */
+class LanguageCourseAdapter :
+    ListAdapter<TerminResponse, LanguageCourseAdapter.LanguageCourseViewHolder>(DiffCallback) {
 
+    /**
+     * Diese Methode ist Teil des RecyclerView.Adapter-Lebenszyklus und wird verwendet, um einen neuen ViewHolder zu erstellen.
+     * Sie bläht das Layout für jedes Element der RecyclerView auf und erstellt einen ViewHolder, um das aufgeblähte Layout zu halten.
+     * Der ViewHolder wird dann verwendet, um die Daten für jedes Element zu füllen.
+     *
+     * @param parent: Die ViewGroup, in die die neue Ansicht hinzugefügt wird, nachdem sie an eine Adapterposition gebunden wurde.
+     * @param viewType: Der Ansichtstyp der neuen Ansicht.
+     * @return LanguageCourseViewHolder: Gibt einen neuen ViewHolder zurück, der die Ansicht für jedes Element hält.
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LanguageCourseViewHolder {
-        val binding = LanguageCourseItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding =
+            LanguageCourseItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return LanguageCourseViewHolder(binding)
     }
 
+
+    /**
+     * Diese Methode ist Teil des RecyclerView.Adapter-Lebenszyklus und wird verwendet, um die Daten eines Elements an einen ViewHolder zu binden.
+     * Sie wird aufgerufen, um die Daten für ein neues Element, das in der RecyclerView angezeigt wird, zu füllen.
+     *
+     * @param holder: Der ViewHolder, der die Daten für das Element hält.
+     * @param position: Die Position des Elements in der Datenliste.
+     */
     override fun onBindViewHolder(holder: LanguageCourseViewHolder, position: Int) {
         val kurs = getItem(position)
         holder.bind(kurs)
     }
+
+    /**
+     * LanguageCourseViewHolder ist eine Unterklasse von RecyclerView.ViewHolder.
+     * Ein ViewHolder hält die Ansicht für ein RecyclerView-Element und ermöglicht es, die Daten für dieses Element zu füllen.
+     *
+     * @property binding: Das Binding-Objekt, das Zugriff auf die Ansichten im Layout ermöglicht.
+     */
     inner class LanguageCourseViewHolder(
         val binding: LanguageCourseItemBinding
     ) : RecyclerView.ViewHolder(binding.root) {
 
 
-
+        /**
+         * Diese Methode wird verwendet, um die Daten eines Sprachkurses an einen ViewHolder zu binden.
+         * Sie füllt die Ansichten im ViewHolder mit den Daten aus dem Sprachkurs.
+         *
+         * @param kurs: Der Sprachkurs, dessen Daten an den ViewHolder gebunden werden.
+         */
         fun bind(kurs: TerminResponse) {
 
+            // Setzt den Kurs-Titel. Wenn kein Titel vorhanden ist, wird "N/A" angezeigt.
             binding.langcourseTextView.text = kurs.angebot?.titel ?: "N/A"
 
-
+            // Formatierung und Anzeige des Beginndatums des Kurses.
             kurs.beginn?.let {
                 val beginnString = SimpleDateFormat("dd.MM.yyyy", Locale.GERMANY).format(Date(it))
-                binding.beginnTextView.text = binding.root.context.getString(R.string.begin, beginnString)
+                binding.beginnTextView.text =
+                    binding.root.context.getString(R.string.begin, beginnString)
             } ?: run {
                 binding.beginnTextView.text = binding.root.context.getString(R.string.begin, "N/A")
             }
 
+            // Formatierung und Anzeige des Enddatums des Kurses.
             kurs.ende?.let {
                 val endeString = SimpleDateFormat("dd.MM.yyyy", Locale.GERMANY).format(Date(it))
                 binding.endeTextView.text = binding.root.context.getString(R.string.end, endeString)
@@ -60,14 +104,21 @@ class LanguageCourseAdapter : ListAdapter<TerminResponse, LanguageCourseAdapter.
                 binding.endeTextView.text = binding.root.context.getString(R.string.end, "N/A")
             }
 
-
+            // Anzeige der Kursbeschreibung. Links werden interaktiv gestaltet.
             kurs.angebot?.inhalt?.let { bemerkung ->
-                val spannableContent = SpannableString(Html.fromHtml(bemerkung, Html.FROM_HTML_MODE_COMPACT))
+                val spannableContent =
+                    SpannableString(Html.fromHtml(bemerkung, Html.FROM_HTML_MODE_COMPACT))
                 Patterns.WEB_URL.matcher(spannableContent).apply {
                     while (find()) {
                         val url = spannableContent.substring(start(), end())
                         spannableContent.setSpan(object : ClickableSpan() {
-                            override fun onClick(widget: View) = widget.context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+                            override fun onClick(widget: View) = widget.context.startActivity(
+                                Intent(
+                                    Intent.ACTION_VIEW,
+                                    Uri.parse(url)
+                                )
+                            )
+
                             override fun updateDrawState(ds: TextPaint) {
                                 super.updateDrawState(ds)
                                 ds.isUnderlineText = true
@@ -82,78 +133,86 @@ class LanguageCourseAdapter : ListAdapter<TerminResponse, LanguageCourseAdapter.
                 }
 
 
-
+                // Setzt den Kostenwert des Kurses. Wenn kein Wert vorhanden ist, wird "N/A" angezeigt.
                 kurs.kostenWert?.let { kosten ->
                     val kostenText = "${kosten}€"
-                    binding.kostenTextView.text = binding.root.context.getString(R.string.cost, kostenText)
+                    binding.kostenTextView.text =
+                        binding.root.context.getString(R.string.cost, kostenText)
                 } ?: run {
-                    binding.kostenTextView.text = binding.root.context.getString(R.string.cost, "N/A")
+                    binding.kostenTextView.text =
+                        binding.root.context.getString(R.string.cost, "N/A")
                 }
 
 
-
-
+                // Zeigt an, ob der Kurs gefördert wird oder nicht.
                 binding.foerderungTextView.text = if (kurs.foerderung == true) {
                     binding.root.context.getString(R.string.funded)
                 } else {
                     binding.root.context.getString(R.string.not_funded)
                 }
 
+
+                // Anzeige der Art des Abschlusses und der Zielgruppe, HTML-Format wird berücksichtigt.
                 kurs.angebot.abschlussart?.let { abschlussart ->
                     val formattedHtml = Html.fromHtml(abschlussart, Html.FROM_HTML_MODE_COMPACT)
-                    binding.abschlussArtTextView.text = binding.root.context.getString(R.string.typeofdegree, formattedHtml)
+                    binding.abschlussArtTextView.text =
+                        binding.root.context.getString(R.string.typeofdegree, formattedHtml)
                 } ?: run {
-                    binding.abschlussArtTextView.text = binding.root.context.getString(R.string.typeofdegree, "N/A")
+                    binding.abschlussArtTextView.text =
+                        binding.root.context.getString(R.string.typeofdegree, "N/A")
                 }
 
                 kurs.angebot.zielgruppe?.let { zielgruppe ->
                     val formattedHtml = Html.fromHtml(zielgruppe, Html.FROM_HTML_MODE_COMPACT)
-                    binding.zielGruppeTextView.text = binding.root.context.getString(R.string.targetgroup, formattedHtml)
+                    binding.zielGruppeTextView.text =
+                        binding.root.context.getString(R.string.targetgroup, formattedHtml)
                 } ?: run {
-                    binding.zielGruppeTextView.text = binding.root.context.getString(R.string.targetgroup, "N/A")
+                    binding.zielGruppeTextView.text =
+                        binding.root.context.getString(R.string.targetgroup, "N/A")
                 }
 
 
-
-
-
-
-
+                // Formatierung und Anzeige des Anmeldeschlusses des Kurses.
                 kurs.anmeldeschluss?.let {
-                    val anmeldeschlussString = SimpleDateFormat("dd.MM.yyyy", Locale.GERMANY).format(Date(it))
-                    binding.anmeldeSchlussTextView.text = binding.root.context.getString(R.string.anmeldeschluss_format, anmeldeschlussString)
+                    val anmeldeschlussString =
+                        SimpleDateFormat("dd.MM.yyyy", Locale.GERMANY).format(Date(it))
+                    binding.anmeldeSchlussTextView.text = binding.root.context.getString(
+                        R.string.anmeldeschluss_format,
+                        anmeldeschlussString
+                    )
                 } ?: run {
-                    binding.anmeldeSchlussTextView.text = binding.root.context.getString(R.string.anmeldeschluss_format, "N/A")
+                    binding.anmeldeSchlussTextView.text =
+                        binding.root.context.getString(R.string.anmeldeschluss_format, "N/A")
                 }
 
 
-
-
-
-
-                // Toggle-Verhalten für die Erweiterungsansicht
-            binding.langcourseTextView.setOnClickListener {
-                binding.expandableView.visibility = if (binding.expandableView.visibility == View.VISIBLE) {
-                    View.GONE
-                } else {
-                    View.VISIBLE
+                // Setzt einen OnClickListener auf den TextView, der den Kursnamen anzeigt
+                binding.langcourseTextView.setOnClickListener {
+                    binding.expandableView.visibility =
+                        if (binding.expandableView.visibility == View.VISIBLE) {
+                            View.GONE
+                        } else {
+                            View.VISIBLE
+                        }
                 }
-            }
 
             }
 
 
-
-
-
+            // Aufruf der Methode 'animateBounce()'
             animateBounce()
         }
 
+        /**
+         * Diese Methode wird aufgerufen, um eine Bounce-Animation auf dem ViewHolder auszuführen.
+         * Die Animation verändert die Y-Position des ViewHolders, um einen Bounce-Effekt zu erzeugen.
+         */
         private fun animateBounce() {
             // Animiere die Y-Position des ViewHolders
             ObjectAnimator.ofFloat(itemView, "translationY", -100f, 0f).apply {
                 duration = 1000  // Dauer der Animation in Millisekunden
-                interpolator = BounceInterpolator()  // Verwendet den BounceInterpolator für den Bounce-Effekt
+                interpolator =
+                    BounceInterpolator()  // Verwendet den BounceInterpolator für den Bounce-Effekt
                 start()
             }
         }
@@ -161,11 +220,37 @@ class LanguageCourseAdapter : ListAdapter<TerminResponse, LanguageCourseAdapter.
     }
 
 
+    /**
+     * Dies ist ein Begleitobjekt namens DiffCallback, das DiffUtil.ItemCallback<TerminResponse> erweitert.
+     * DiffUtil ist eine Hilfsklasse, die den Unterschied zwischen zwei Listen berechnet und eine Liste von Update-Operationen ausgibt,
+     * die die erste Liste in die zweite umwandelt.
+     * Es kann verwendet werden, um Updates für einen RecyclerView Adapter zu berechnen.
+     * DiffUtil verwendet Eugene W. Myers's Differenzalgorithmus, um die minimale Anzahl von Updates zu berechnen, die benötigt werden, um eine Liste in eine andere umzuwandeln.
+     *
+     */
     companion object DiffCallback : DiffUtil.ItemCallback<TerminResponse>() {
+
+        /**
+         * Wird aufgerufen, um zu überprüfen, ob zwei Objekte das gleiche Element darstellen.
+         * Wenn Ihre Elemente beispielsweise eindeutige IDs haben, sollte diese Methode ihre ID-Gleichheit überprüfen.
+         *
+         * @param oldItem Das TerminResponse-Element aus der alten Liste.
+         * @param newItem Das TerminResponse-Element aus der neuen Liste.
+         * @return True, wenn die beiden Elemente das gleiche Objekt darstellen, oder false, wenn sie unterschiedlich sind.
+         */
         override fun areItemsTheSame(oldItem: TerminResponse, newItem: TerminResponse): Boolean {
             return oldItem.id == newItem.id
         }
 
+        /**
+         * Wird aufgerufen, um zu überprüfen, ob zwei Elemente die gleichen Daten haben.
+         * Diese Information wird verwendet, um zu erkennen, ob sich der Inhalt eines Elements geändert hat.
+         * Diese Methode wird nur aufgerufen, wenn areItemsTheSame(T, T) für diese Elemente true zurückgibt.
+         *
+         * @param oldItem Das TerminResponse-Element aus der alten Liste.
+         * @param newItem Das TerminResponse-Element aus der neuen Liste.
+         * @return True, wenn der Inhalt der Elemente gleich ist, oder false, wenn sie unterschiedlich sind.
+         */
         override fun areContentsTheSame(oldItem: TerminResponse, newItem: TerminResponse): Boolean {
             return oldItem == newItem
         }
--- app/src/main/java/com/example/promigrate/ui/LanguageCourseFragment.kt
+++ app/src/main/java/com/example/promigrate/ui/LanguageCourseFragment.kt
@@ -15,30 +15,58 @@ import com.example.promigrate.databinding.FragmentLanguageCourseBinding
 
 class LanguageCourseFragment : Fragment() {
 
+    // Die Verwendung von activityViewModels() bietet Zugriff auf das ViewModel, das von der
+    // zugehörigen Activity genutzt wird.
     private val viewModel: MainViewModel by activityViewModels()
 
-    private  var binding: FragmentLanguageCourseBinding? = null
+    // Binding-Objekt, das Zugriff auf die im XML definierten Views ermöglicht.
+    // Wird nullable gehalten, da es zwischen onCreateView und onDestroyView null sein kann.
+    private var binding: FragmentLanguageCourseBinding? = null
 
     private lateinit var languageCourseAdapter: LanguageCourseAdapter
 
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    /**
+     * Initialisiert die Benutzeroberfläche des Fragments, indem das entsprechende Layout aufgeblasen wird.
+     *
+     * @param inflater: Das LayoutInflater-Objekt, das zum Aufblasen des Layouts des Fragments verwendet wird.
+     * @param container: Die übergeordnete ViewGroup, in den die neue Ansicht eingefügt wird.
+     * @param savedInstanceState: Ein Bundle, das den Zustand des Fragments gespeichert hat.
+     * @return :Die erstellte View für das Fragment.
+     */
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         binding = FragmentLanguageCourseBinding.inflate(inflater, container, false)
         return binding!!.root
     }
 
+    /**
+     * Wird aufgerufen, nachdem die Ansicht des Fragments und seine hierarchische Struktur instanziiert wurden.
+     * In dieser Methode werden weitere UI-Initialisierungen vorgenommen und Listener für UI-Elemente eingerichtet.
+     *
+     * @param view: Die erstellte Ansicht des Fragments.
+     * @param savedInstanceState: Ein Bundle, das den Zustand des Fragments gespeichert hat.
+     */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Erstellt eine Instanz des LanguageCourseAdapter
         languageCourseAdapter = LanguageCourseAdapter()
         binding!!.rvLanguageCourses.apply {
+            // Setzt den Adapter für das RecyclerView
             adapter = languageCourseAdapter
+            // Setzt den LayoutManager für das RecyclerView
             layoutManager = LinearLayoutManager(context)
         }
 
+        // Beobachtet die userProfileData im ViewModel
         viewModel.userProfileData.observe(viewLifecycleOwner) { userProfile ->
+            // Konvertiert den gewünschten Standort und das Sprachniveau
             val location = userProfile?.desiredLocation?.let { convertLocation(it) }
             val sprachniveau = userProfile?.languageLevel?.let { convertLanguageLevel(it) }
+            // Wenn Standort und Sprachniveau nicht null sind, werden Bildungsangebote abgerufen
             if (location != null) {
                 if (sprachniveau != null) {
                     viewModel.fetchEducationalOffers("MC", location, sprachniveau, 0)
@@ -46,24 +74,34 @@ class LanguageCourseFragment : Fragment() {
             }
         }
 
+        // Beobachtet die educationaloffers im ViewModel
         viewModel.educationaloffers.observe(viewLifecycleOwner) { angebote ->
+            // Übersetzt die Bildungsangebote
             viewModel.translateEducationalOffers(angebote) { translatedAngebote ->
+                // Aktualisiert die Liste im Adapter mit den übersetzten Angeboten
                 languageCourseAdapter.submitList(translatedAngebote)
             }
         }
 
-
-
-
-
-
+        // Setzt einen OnClickListener auf den "backToDashboardButton"
         binding!!.backToDashboardButton.setOnClickListener {
-            val action = LanguageCourseFragmentDirections.actionLanguageCourseFragmentToVocabularyLearningFragment()
+            // Erstellt eine Navigationsaktion zum VocabularyLearningFragment
+            val action =
+                LanguageCourseFragmentDirections.actionLanguageCourseFragmentToVocabularyLearningFragment()
+            // Navigiert zum VocabularyLearningFragment
             findNavController().navigate(action)
         }
     }
 
-
+    /**
+     * Diese Funktion konvertiert den gewünschten Standort (eine Stadt in Deutschland oder Österreich) in eine spezielle Zeichenkette.
+     * Diese Zeichenkette enthält den Namen der Stadt, die Postleitzahl und die geographischen Koordinaten (Längen- und Breitengrad).
+     * Diese spezielle Zeichenkette wird dann von anderen Funktionen verwendet, um Bildungsangebote in der Nähe des gewünschten Standorts zu suchen.
+     *
+     * @param desiredLocation: Der gewünschte Standort, der konvertiert werden soll. Es sollte der Name einer Stadt in Deutschland oder Österreich sein.
+     * @return: Eine Zeichenkette, die den Namen der Stadt, die Postleitzahl und die geographischen Koordinaten enthält.
+     *         Wenn der gewünschte Standort nicht in der Liste der unterstützten Städte enthalten ist, wird eine leere Zeichenkette zurückgegeben.
+     */
     private fun convertLocation(desiredLocation: String): String {
         // Beispiel für eine einfache Konvertierung
         return when (desiredLocation) {
@@ -168,13 +206,21 @@ class LanguageCourseFragment : Fragment() {
             "Zwickau" -> "Zwickau_08056_12.4956443_50.7090527"
 
 
-            else -> ""
+            else -> ""// Wenn nichts übereinstimmt, wird ein leerer String übergeben
         }
     }
 
+    /**
+     * Diese Funktion konvertiert das Sprachniveau in eine spezielle Zeichenkette.
+     * Diese Zeichenkette wird dann von anderen Funktionen verwendet, um Bildungsangebote auf der Grundlage des Sprachniveaus zu suchen.
+     *
+     * @param languageLevel: Das Sprachniveau, das konvertiert werden soll. Es sollte einer der Werte sein, die in den Ressourcen-Strings definiert sind.
+     * @return: Eine Zeichenkette, die das Sprachniveau repräsentiert.
+     *          Wenn das Sprachniveau nicht in der Liste der unterstützten Niveaus enthalten ist, wird ein Standardwert zurückgegeben.
+     */
     private fun convertLanguageLevel(languageLevel: String): String {
         return when (languageLevel) {
-            getString(R.string.beginner) -> "MC01 2" // Angenommen, es gibt keine Kurse unter A2.
+            getString(R.string.beginner) -> "MC01 2" // Es gibt keine Kurse unter A2.
             getString(R.string.basic_knowledge) -> "MC01 2" // Konvertierung für A2.
             getString(R.string.intermediate) -> "MC01 3" // Konvertierung für B1.
             getString(R.string.independent) -> "MC01 4" // Konvertierung für B2.
@@ -184,15 +230,15 @@ class LanguageCourseFragment : Fragment() {
         }
     }
 
+    /**
+     * Wird aufgerufen, wenn die View-Hierarchie des Fragments zerstört wird.
+     * Hier wird das Binding-Objekt auf null gesetzt, um Memory Leaks zu vermeiden,
+     * da das Binding-Objekt eine Referenz auf die View hält, welche nicht länger existiert.
+     */
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
     }
-
-
-
-
-
 
 
 }
