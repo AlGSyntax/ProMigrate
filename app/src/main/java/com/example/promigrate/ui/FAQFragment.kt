e5eaaaf76ce5347a3744a74cb3bef39306b8f674
alvaro <adelcastifo@gmail.com>
2024-03-12 11:53:10 +0100

    Adding documentation and comments in the FAQFragment finished .

--- app/src/main/java/com/example/promigrate/ui/FAQFragment.kt
+++ app/src/main/java/com/example/promigrate/ui/FAQFragment.kt
@@ -14,28 +14,60 @@ import com.example.promigrate.R
 import com.example.promigrate.databinding.FragmentFaqBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
-
+/**
+ * FAQFragment ist ein Fragment, das eine Liste von häufig gestellten Fragen (FAQs) und deren Antworten anzeigt.
+ * Es bietet dem Benutzer auch die Möglichkeit, zur Einstellungsseite zurückzukehren, zum Anfang der Seite zu blättern,
+ * zu einer bestimmten Antwort zu blättern, wenn eine Frage angeklickt wird, und sein Konto zu löschen.
+ */
 class FAQFragment : Fragment() {
 
+    // Die Verwendung von activityViewModels() bietet Zugriff auf das ViewModel, das von der
+    // zugehörigen Activity genutzt wird.
     private val viewModel: MainViewModel by activityViewModels()
+
+    // Binding-Objekt, das Zugriff auf die im XML definierten Views ermöglicht.
+    // Wird nullable gehalten, da es zwischen onCreateView und onDestroyView null sein kann.
     private var binding: FragmentFaqBinding? = null
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    /**
+     * Wird aufgerufen, um die Ansicht des Fragments zu erstellen.
+     * Es erstellt das Binding-Objekt und gibt die Wurzelansicht zurück.
+     *
+     * @param inflater: Der LayoutInflater, der zum Aufblasen der Ansichten verwendet wird.
+     * @param container: Die übergeordnete Ansicht, in die das Fragment eingefügt wird.
+     * @param savedInstanceState: Ein Bundle, das den Zustand des Fragments gespeichert hat.
+     * @return :Die erstellte Ansicht des Fragments.
+     */
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         binding = FragmentFaqBinding.inflate(inflater, container, false)
         return binding!!.root
     }
 
+    /**
+     * Wird aufgerufen, nachdem die Ansicht des Fragments und seine hierarchische Struktur instanziiert wurden.
+     * In dieser Methode werden weitere UI-Initialisierungen vorgenommen und Listener für UI-Elemente eingerichtet.
+     *
+     * @param view: Die erstellte Ansicht des Fragments.
+     * @param savedInstanceState: Ein Bundle, das den Zustand des Fragments gespeichert hat.
+     */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Setzt einen OnClickListener auf den backButton. Wenn er angeklickt wird, navigiert er zum SettingsFragment.
         binding!!.backButton.setOnClickListener {
             findNavController().navigate(FAQFragmentDirections.actionFAQFragmentToSettingsFragment())
         }
 
+        // Setzt einen OnClickListener auf den scrollToTopFromRightArrow. Wenn er angeklickt wird, scrollt er an den oberen Rand der scrollView.
         binding!!.scrollToTopFromRightArrow.setOnClickListener {
             binding!!.scrollView.scrollTo(0, 0)
         }
 
+        // Listen von Fragen und Antworten
         val questions = listOf(
             binding!!.question1, binding!!.question2, binding!!.question3, binding!!.question4,
             binding!!.question5, binding!!.question6, binding!!.question7
@@ -46,12 +78,14 @@ class FAQFragment : Fragment() {
             binding!!.answer5, binding!!.answer6, binding!!.answer7
         )
 
+        // Setzt einen OnClickListener auf jede Frage. Wenn eine Frage angeklickt wird, blättert sie zur entsprechenden Antwort.
         questions.zip(answers).forEach { (question, answer) ->
             question.setOnClickListener {
                 binding!!.scrollView.scrollTo(0, answer.y.toInt())
             }
         }
 
+        // Setzt einen OnClickListener auf die Frage8. Wenn sie angeklickt wird, wird ein Dialog zur Bestätigung der Kontolöschung angezeigt.
         binding!!.question8.setOnClickListener {
             MaterialAlertDialogBuilder(requireContext(), R.style.CustomAlertDialog)
                 .setTitle(getString(R.string.delete_account))
@@ -66,23 +100,31 @@ class FAQFragment : Fragment() {
                 .show()
         }
 
+        // Legt den Text und die Bewegungsmethode von answer3 fest.
         binding!!.answer3.apply {
             text = getString(R.string.answer3)
             movementMethod = LinkMovementMethod.getInstance()
         }
 
-
+        // Beobachtet die deleteAccountStatus LiveData im ViewModel. Wenn die Kontolöschung erfolgreich ist, navigiert es zum LanguageSelectionFragment.
+        // Schlägt die Kontolöschung fehl, wird eine Toast-Meldung angezeigt.
         viewModel.deleteAccountStatus.observe(viewLifecycleOwner) { isSuccess ->
             isSuccess?.let {
                 if (it) {
                     findNavController().navigate(R.id.action_FAQFragment_to_languageSelectionFragment)
                 } else {
-                    Toast.makeText(context, R.string.account_deletion_failed, Toast.LENGTH_SHORT).show()
+                    Toast.makeText(context, R.string.account_deletion_failed, Toast.LENGTH_SHORT)
+                        .show()
                 }
             }
         }
     }
 
+    /**
+     * Wird aufgerufen, wenn die View-Hierarchie des Fragments zerstört wird.
+     * Hier wird das Binding-Objekt auf null gesetzt, um Memory Leaks zu vermeiden,
+     * da das Binding-Objekt eine Referenz auf die View hält, welche nicht länger existiert.
+     */
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
--- app/src/main/java/com/example/promigrate/ui/SettingsFragment.kt
+++ app/src/main/java/com/example/promigrate/ui/SettingsFragment.kt
@@ -1,7 +1,6 @@
 package com.example.promigrate.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -20,38 +19,98 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
+/**
+ * SettingsFragment ist ein Fragment, das die Einstellungsseite der App darstellt.
+ * Es ermöglicht dem Benutzer, sein Profilbild zu ändern, Feedback zu geben und sich abzumelden.
+ * Es verwendet ein Binding-Objekt, um auf die im XML definierten Ansichten zuzugreifen.
+ */
 class SettingsFragment : Fragment() {
 
-    private val TAG = "SettingsFragment"
 
-
+    // Die Verwendung von activityViewModels() bietet Zugriff auf das ViewModel, das von der
+    // zugehörigen Activity genutzt wird.
     private val viewModel: MainViewModel by activityViewModels()
+
+    // Binding-Objekt, das Zugriff auf die im XML definierten Views ermöglicht.
+    // Wird nullable gehalten, da es zwischen onCreateView und onDestroyView null sein kann.
     private var binding: FragmentSettingsBinding? = null
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    /**
+     * Wird aufgerufen, um die Ansicht des Fragments zu erstellen.
+     * Es erstellt das Binding-Objekt und gibt die Wurzelansicht zurück.
+     *
+     * @param inflater: Der LayoutInflater, der zum Aufblasen der Ansichten verwendet wird.
+     * @param container: Die übergeordnete Ansicht, in die das Fragment eingefügt wird.
+     * @param savedInstanceState: Ein Bundle, das den Zustand des Fragments gespeichert hat.
+     * @return :Die erstellte Ansicht des Fragments.
+     */
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         binding = FragmentSettingsBinding.inflate(inflater, container, false)
         return binding!!.root
     }
 
+    /**
+     * Wird aufgerufen, nachdem die Ansicht des Fragments und seine hierarchische Struktur instanziiert wurden.
+     * In dieser Methode werden weitere UI-Initialisierungen vorgenommen und Listener für UI-Elemente eingerichtet.
+     *
+     * @param view: Die erstellte Ansicht des Fragments.
+     * @param savedInstanceState: Ein Bundle, das den Zustand des Fragments gespeichert hat.
+     */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Aufruf der Methode loadCurrentProfileImage()
         loadCurrentProfileImage()
 
-        val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
-            if (uri != null) {
-                Glide.with(this).load(uri).circleCrop().into(binding!!.profileImage)
-                viewModel.updateProfileImage(uri)
-            } else {
-                Log.d(TAG, "No image selected")
+        /**
+         * Dieser Codeblock registriert einen Callback für die Auswahl eines Bildes aus der Galerie.
+         * Der Callback wird ausgelöst, wenn ein Bild ausgewählt wird, und die URI des Bildes wird als Parameter übergeben.
+         *
+         * Wenn die URI nicht null ist (was bedeutet, dass ein Bild ausgewählt wurde), wird das Bild mit der Glide-Bibliothek in das ImageView profileImage geladen.
+         * Das Bild wird auch im ViewModel aktualisiert, was normalerweise die Änderung an die zugrunde liegende Datenquelle weiterleitet.
+         *
+         * Wenn die URI null ist (was bedeutet, dass kein Bild ausgewählt wurde), wird eine Toast-Nachricht angezeigt, um den Benutzer darüber zu informieren, dass keine Mediendatei ausgewählt wurde.
+         */
+        val pickMedia =
+            registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+                if (uri != null) {
+                    Glide.with(this).load(uri).circleCrop().into(binding!!.profileImage)
+                    viewModel.updateProfileImage(uri)
+                } else {
+                    Toast.makeText(context, "Keine Mediendatei ausgewählt", Toast.LENGTH_SHORT)
+                        .show()
+                }
             }
-        }
 
+        /**
+         * Dieser Codeblock setzt einen OnClickListener auf die ImageView profileImage.
+         * Wenn die ImageView angeklickt wird, startet sie den Startvorgang für das Ergebnis der pickMedia-Aktivität.
+         * Der Launcher ist so konfiguriert, dass er nur Bilder aus den visuellen Medien (Galerie, etc.) auswählt.
+         * Dies ermöglicht dem Benutzer, ein neues Profilbild aus den visuellen Medien seines Geräts auszuwählen.
+         */
         binding!!.profileImage.setOnClickListener {
             pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
         }
 
 
+        /**
+         * Dieser Codeblock beobachtet Änderungen im userProfileData LiveData-Objekt im ViewModel.
+         * Wenn sich die userProfileData ändert, wird die Benutzeroberfläche entsprechend aktualisiert.
+         *
+         * Wenn das userProfile-Objekt nicht null ist, wird das Profilbild mit Hilfe der Glide-Bibliothek in die profileImage ImageView geladen.
+         * Wenn die URL des Profilbildes null ist, lädt Glide kein Bild.
+         * Der Name des Benutzers wird ebenfalls aus dem userProfile-Objekt abgerufen und in der userGreeting TextView angezeigt.
+         * Wenn der Name des Benutzers null ist, wird eine Standardbegrüßung angezeigt.
+         *
+         * Die Eigenschaft languageLevel des userProfile-Objekts wird ebenfalls beachtet.
+         * Wenn languageLevel nicht null ist, wird der Wert des languageLevelSliders so gesetzt, dass er dem Sprachniveau des Benutzers entspricht.
+         * Das Sprachniveau wird auch in der languageLevelText TextView angezeigt.
+         * Wenn languageLevel null ist, wird der Wert des Sliders auf einen Standardwert gesetzt, und die TextView zeigt einen Standardtext an.
+         */
         viewModel.userProfileData.observe(viewLifecycleOwner) { userProfile ->
             userProfile?.let {
                 Glide.with(this)
@@ -78,16 +137,40 @@ class SettingsFragment : Fragment() {
             }
         }
 
+        /**
+         * Dieser Codeblock setzt einen OnClickListener auf den gotofaqbtn Button.
+         * Wenn der Button angeklickt wird, navigiert er zum FAQFragment unter Verwendung der findNavController() Methode und der actionSettingsFragmentToFAQFragment() Aktion.
+         * Dies ermöglicht dem Benutzer, vom SettingsFragment zu FAQFragment zu navigieren.
+         */
         binding!!.gotofaqbtn.setOnClickListener {
             findNavController().navigate(SettingsFragmentDirections.actionSettingsFragmentToFAQFragment())
         }
 
 
-
+        /**
+         * Dieser Codeblock setzt einen OnClickListener auf die Schaltfläche gotofeedbackbtn.
+         * Wenn der Button angeklickt wird, wird ein Dialog-Layout für die Übermittlung von Feedback mit Hilfe der FeedbackDialogLayoutBinding erstellt.
+         *
+         * Ein MaterialAlertDialogBuilder wird verwendet, um den Dialog zu erstellen. Der Titel des Dialogs wird auf "Feedback" gesetzt.
+         * Das aufgeblähte Layout wird als Ansicht für den Dialog festgelegt.
+         *
+         * Das Dialogfeld hat zwei Schaltflächen - eine positive und eine negative Schaltfläche.
+         * Die positive Schaltfläche ist mit "Sendfeedback" beschriftet. Wenn diese Schaltfläche angeklickt wird, werden die Bewertungen für Design und Funktionalität abgerufen,
+         * und die allgemeine Meinung aus dem Layout des Dialogs. Die Gesamtbewertung errechnet sich aus dem Durchschnitt der Bewertungen für Design und Funktionalität.
+         *
+         * Die ID des Benutzers wird von FirebaseAuth abgerufen. Wenn der Benutzer nicht angemeldet ist (d.h. die Benutzer-ID ist null), wird ein leerer String als Benutzer-ID verwendet.
+         *
+         * Das Feedback (einschließlich Benutzer-ID, Designbewertung, Funktionsbewertung, Gesamtbewertung und allgemeines Feedback) wird mit der saveFeedback-Methode des ViewModel gespeichert.
+         * Eine Toast-Meldung wird angezeigt, um den Benutzer darüber zu informieren, dass das Feedback gesendet wurde.
+         *
+         * Die negative Schaltfläche ist mit 'Abbrechen' beschriftet. Sie hat keinen OnClickListener, so dass sie den Dialog einfach beendet, wenn sie angeklickt wird.
+         *
+         * Schließlich wird der Dialog mit der Methode show() angezeigt.
+         */
         binding!!.gotofeedbackbtn.setOnClickListener {
             val feedbackBinding = FeedbackDialogLayoutBinding.inflate(layoutInflater)
 
-            MaterialAlertDialogBuilder(it.context,R.style.CustomAlertDialog)
+            MaterialAlertDialogBuilder(it.context, R.style.CustomAlertDialog)
                 .setTitle(R.string.feedback)
                 .setView(feedbackBinding.root)
                 .setPositiveButton(R.string.sendfeedback) { _, _ ->
@@ -95,7 +178,7 @@ class SettingsFragment : Fragment() {
                     val functionalityFeedback = feedbackBinding.ratingBarFunctionality.rating
                     val generalOpinion = feedbackBinding.editTextGeneralOpinion.text.toString()
 
-                    // Calculate the overall rating as the average of design and functionality ratings
+                    // Berechnung der Gesamtbewertung als Durchschnitt aus Design- und Funktionsbewertung
                     val overallRating = (designFeedback + functionalityFeedback) / 2
 
                     // Die UserID sollte dynamisch aus dem Benutzerkontext abgerufen werden
@@ -115,7 +198,16 @@ class SettingsFragment : Fragment() {
                 .show()
         }
 
-
+        /**
+         * Dieser Codeblock setzt einen OnChangeListener auf den languageLevelSlider Slider.
+         * Wenn sich der Wert des Sliders ändert, wird der neue Wert mit der saveLanguageLevelToFirebase-Methode des ViewModels in Firebase gespeichert.
+         *
+         * Der neue Wert wird auch zur Aktualisierung der TextView languageLevelText verwendet.
+         * Der Wert wird in eine ganze Zahl umgewandelt und in einem When-Ausdruck verwendet, um die entsprechende Sprachlevel-String-Ressource zu bestimmen.
+         * Die String-Ressource wird dann abgerufen und als Text des TextViews festgelegt.
+         *
+         * Entspricht der Wert keiner der vordefinierten Sprachstufen, wird die String-Ressource für 'undefined' verwendet.
+         */
         binding!!.languageLevelSlider.addOnChangeListener { _, value, _ ->
 
             viewModel.saveLanguageLevelToFirebase(value.toString())
@@ -135,16 +227,22 @@ class SettingsFragment : Fragment() {
         }
 
 
-
-
-
-
-        binding!!.backtodashbtn3.setOnClickListener{
+        /**
+         * Dieser Codeblock setzt einen OnClickListener auf die Schaltfläche backtodashbtn3.
+         * Wenn die Schaltfläche angeklickt wird, navigiert sie mithilfe der findNavController()-Methode und der actionSettingsFragmentToDashboardFragment()-Aktion zum DashboardFragment.
+         * Dies ermöglicht dem Benutzer, von der Einstellungsseite zurück zur Dashboard-Seite zu navigieren.
+         */
+        binding!!.backtodashbtn3.setOnClickListener {
             findNavController().navigate(SettingsFragmentDirections.actionSettingsFragmentToDashboardFragment())
         }
 
-
-        binding!!.logoutbtn.setOnClickListener{
+        /**
+         * Dieser Codeblock setzt einen OnClickListener auf die Schaltfläche logoutbtn.
+         * Wenn der Button angeklickt wird, ruft er die logout()-Methode des ViewModels auf, um den Benutzer abzumelden.
+         * Anschließend wird mit der findNavController()-Methode und der actionSettingsFragmentToLoginFragment()-Aktion zum LoginFragment navigiert.
+         * Dadurch kann sich der Benutzer abmelden und wird zur Login-Seite weitergeleitet.
+         */
+        binding!!.logoutbtn.setOnClickListener {
             viewModel.logout()
             findNavController().navigate(SettingsFragmentDirections.actionSettingsFragmentToLoginFragment())
         }
@@ -152,27 +250,40 @@ class SettingsFragment : Fragment() {
 
     }
 
+    /**
+     * Lädt das aktuelle Profilbild des Benutzers.
+     * Es holt die Benutzer-ID von FirebaseAuth und verwendet sie, um das Dokument des Benutzers in Firestore abzurufen.
+     * Wenn das Dokument existiert, wird das Profilbild-URL abgerufen und verwendet, um das Bild mit Glide zu laden und in der ImageView anzuzeigen.
+     * Wenn das Dokument nicht existiert oder das Profilbild-URL leer ist, wird ein Platzhalterbild angezeigt.
+     */
     private fun loadCurrentProfileImage() {
         val userId = FirebaseAuth.getInstance().currentUser?.uid
         if (userId != null) {
-            FirebaseFirestore.getInstance().collection("user").document(userId).get().addOnSuccessListener { document ->
-                if (document != null && document.exists()) {
-                    val imageUrl = document.getString("profilePicture")
-                    if (!imageUrl.isNullOrEmpty()) {
-                        Glide.with(this)
-                            .load(imageUrl)
-                            .circleCrop()
-                            .into(binding!!.profileImage)
-                    } else {
-                        R.drawable.elevationtodolistrelocation// passendes palceholder suchen
+            FirebaseFirestore.getInstance().collection("user").document(userId).get()
+                .addOnSuccessListener { document ->
+                    if (document != null && document.exists()) {
+                        val imageUrl = document.getString("profilePicture")
+                        if (!imageUrl.isNullOrEmpty()) {
+                            Glide.with(this)
+                                .load(imageUrl)
+                                .circleCrop()
+                                .into(binding!!.profileImage)
+                        } else {
+                            // passenden Platzhalter suchen
+                            binding!!.profileImage.setImageResource(R.drawable.elevationtodolistrelocation)
+                        }
                     }
-                }
-            }.addOnFailureListener {
-                Log.e(TAG, "Fehler beim Laden des Profilbildes", it)
+                }.addOnFailureListener {
+                Toast.makeText(context, R.string.errorload, Toast.LENGTH_SHORT).show()
             }
         }
     }
 
+    /**
+     * Wird aufgerufen, wenn die View-Hierarchie des Fragments zerstört wird.
+     * Hier wird das Binding-Objekt auf null gesetzt, um Memory Leaks zu vermeiden,
+     * da das Binding-Objekt eine Referenz auf die View hält, welche nicht länger existiert.
+     */
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
